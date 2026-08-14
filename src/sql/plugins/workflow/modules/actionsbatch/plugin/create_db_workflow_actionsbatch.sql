d2b72d206cbc533a0a5ebad7b021195cf98bc46e
seboo <25958061+seboo@users.noreply.github.com>
2026-08-14 11:37:54 +0200

    add liquibase headers in sql script

--- src/sql/plugins/workflow/modules/actionsbatch/plugin/create_db_workflow_actionsbatch.sql
+++ src/sql/plugins/workflow/modules/actionsbatch/plugin/create_db_workflow_actionsbatch.sql
@@ -1,3 +1,6 @@
+-- liquibase formatted sql
+-- changeset workflow-actionsbatch:create_db_workflow_actionsbatch.sql
+-- preconditions onFail:MARK_RAN onError:WARN
 
 DROP TABLE IF EXISTS workflow_task_actions_batch_cf;
 CREATE TABLE workflow_task_actions_batch_cf (
